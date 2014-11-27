d3b9f139231cd32205d0d6f4449599e3aadc6c95
Hraban Luyat <hraban@0brg.net>
2014-11-27 19:36:27 +0100

    gulp watch rebuilds on save file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
+// Copyright © 2014 authors, see package.json
+//
+// all rights reserved
+
 var concat = require('gulp-concat');
 var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var typescript = require('gulp-typescript');
 
-gulp.task('default', function () {
+gulp.task('default', ['typescript']);
+
+gulp.task('watch', ['typescript'], function() {
+    gulp.watch('*.ts', ['typescript']);
+});
+
+gulp.task('typescript', function () {
     gulp.src(['*.ts'])
         .pipe(sourcemaps.init())
         .pipe(typescript({
